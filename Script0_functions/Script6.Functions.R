b012e860ddabfe7f13dd5b7bf64bd58089b76686
gbaldanzi <gabrielbaldanzi@gmail.com>
2021-11-02 14:57:43 +0100

    Update Script6.Functions.R
    Changed the variable name "cor" to "rho". Rho makes it more clear that we are talking about a Spearman correlation

--- Script0_functions/Script6.Functions.R
+++ Script0_functions/Script6.Functions.R
@@ -6,13 +6,17 @@ Clean.Correlation.Results <- function(res){
   if(!any(class(res) %in% "data.table")){setDT(res)}
      
      if(any(names(res) %in% "cor.coefficient")){
-       setnames(res,"cor.coefficient","cor.")
+       setnames(res,"cor.coefficient","rho")
      }
+  
+  if(any(names(res) %in% "cor.")){
+    setnames(res,"cor.","rho")
+  }
      
-     a = c("MGS","exposure","cor.","p.value","q.value","N")   
+     a = c("MGS","exposure","rho","p.value","q.value","N")   
      res = res[,a,with=F]
      
-     res[,cor.:=round(cor.,3)]
+     res[,rho:=round(rho,3)]
      res[q.value>=0.001, q.value:=round(q.value, digits = 3)]
      res[p.value>=0.001, p.value:=round(p.value, digits = 3)]
      
