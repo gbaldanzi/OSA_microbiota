6aa84adc972bc3967f783012b315449731f8ab63
gbaldanzi <gabrielbaldanzi@gmail.com>
2021-11-02 15:08:46 +0100

    New GMM enrichment analysis

---
+++ Script9_enrichment/GMM/Script9_GMM_ea.R
@@ -0,0 +1,75 @@
+# Script 9 - Enrichment analysis using model2 correlation coefficients 
+
+# Gabriel Baldanzi v1 2021-10-25
+
+# Last update: 
+
+# Loading packages 
+pacman::p_load(data.table,ggplot2, tidyr, fgsea,rio, ggvenn)
+
+source("MGS.Enrich.function.R")
+
+# input and output folders 
+  input1 = "/home/baldanzi/Sleep_apnea/Results/"
+  input2 = "/home/baldanzi/Datasets/MGS/original/"
+  output = "/home/baldanzi/Sleep_apnea/Results/"
+  output.plot = "/home/baldanzi/Sleep_apnea/Results/Plots/"
+
+  # Import results 
+  res <- fread(paste0(input1,"cor_all.var_mgs.tsv"))
+  
+  res.list <- list(AHI = res[exposure=="ahi",],
+                   T90 = res[exposure=="t90",],
+                   BMI = res[exposure=="BMI",])
+
+# List of modules ####
+  load(paste0(input2,'MGS_HG3A.GMMs2MGS.RData')) # object = MGS_HG3A.GMMs2MGS
+  list.modules <-  MGS_HG3A.GMMs2MGS
+
+  # Enrichment analysis   
+    # Positive correlations 
+    message("Positive correlations")
+    res.pos = list()[1:3]
+    
+    for(i in 1:3){
+    print(i)
+      
+      res.pos[[i]] <-  MGS.Enrich.Analysis(res.list[[i]],  
+                       p.value.name="p.value",
+                       cor.var.name = "cor.coefficient",
+                       MGS.var.name = "mgs",
+                       enrich.var.list = list.modules,
+                       direction = "positive", 
+                       maxSize = Inf)
+          names(res.pos)[i] <- names(res.list)[i]
+    }
+
+    
+      # Merging with module annotation 
+    
+        res.pos <- do.call(rbind,res.pos)
+        fwrite(res.pos, file = paste0(output,"ea_GMM_pos.tsv"))
+      
+    # Negative correlations
+
+        message("Negative correlations")
+        # Removing the single MGS that is negative correlated and not present in list of modules
+       # res.list <- lapply(res.list,function(x){x[mgs!="HG3A.1213",]})
+        res.neg = list()[1:3]
+        
+        for(i in 1:3){
+          print(i)
+        res.neg[[i]] <-  MGS.Enrich.Analysis(res.list[[i]],  
+                           p.value.name="p.value",
+                           cor.var.name = "cor.coefficient",
+                           MGS.var.name = "mgs",
+                           enrich.var.list = list.modules,
+                           direction = "negative", 
+                           maxSize = Inf)
+        names(res.neg)[i] <- names(res.list)[i]
+        }
+        
+        
+        res.neg <- do.call(rbind, res.neg)
+        fwrite(res.neg, file = paste0(output,"ea_GMM_neg.tsv"))
+        
---
+++ Script9_enrichment/GMM/Script9_GMM_table.res.R
@@ -0,0 +1,39 @@
+# Project: Sleep apnea and gut microbiota
+# Gabriel Baldanzi 
+# Script created in 2021-10-30
+
+# Inferential Statistics 
+
+# This code will produce a table with overrepresentation analysis of GMM modules
+# correlated to AHI, T90 and BMI
+
+
+  pacman::p_load(data.table)
+  
+  # input and output folders 
+  input <-  "/home/baldanzi/Sleep_apnea/Results/"
+  output <-  "/home/baldanzi/Sleep_apnea/Results/"
+  
+  #Importing
+  res.pos <- fread(paste0(input,"ea_GMM_pos.tsv"))
+  res.neg <- fread(paste0(input,"ea_GMM_neg.tsv"))
+  
+  # Import GMM names 
+  gmm.names <- fread('/home/baldanzi/Datasets/MGS/GMM_reference.csv')
+  
+  
+  res.pos <- merge(res.pos, gmm.names, by.x="pathway", by.y="Module")
+  res.neg <- merge(res.neg, gmm.names, by.x="pathway", by.y="Module")
+  
+  #Cleaning
+  res.pos[q.value>=0.001, q.value:=round(q.value, digits = 3)]
+  res.neg[q.value>=0.001, q.value:=round(q.value, digits = 3)]
+  
+  res.pos <-  res.pos[order(q.value) & q.value<0.05,.(exposure,pathway,Name,HL1, HL2,pval,q.value)]
+  res.neg <-  res.neg[order(q.value) & q.value<0.05,.(exposure,pathway,Name,HL1, HL2,pval,q.value)]
+
+  #Saving 
+  saveRDS(res.pos,file=paste0(output,"ea_GMM_table_pos.rds"))
+  saveRDS(res.neg,file=paste0(output,"ea_GMM_table_neg.rds"))
+
+  
