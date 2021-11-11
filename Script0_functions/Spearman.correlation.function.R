85c0e6fd42ab15d000cf5b1b88ed15c1d0c214c5
gbaldanzi <gabrielbaldanzi@gmail.com>
2021-11-11 17:58:27 +0100

    Made adjustments to the function so that it will not transform the inputed data into a data.frame if it was originally a data.table

--- Script0_functions/Spearman.correlation.function.R
+++ Script0_functions/Spearman.correlation.function.R
@@ -4,32 +4,35 @@
 # Function: Spearman correlation 
 
 spearman.function = function(x1, x2, covari=NULL, data){
-  if(any(class(data) %in% "data.table")){setDF(data)}
-  numeric.covari = names(data[covari])[sapply(data[covari],is.numeric)]
-  factor.covari =  names(data[covari])[sapply(data[covari],is.factor)]
-  factor.covari = c(factor.covari,
-                    names(data[covari])[sapply(data[covari],is.character)])
-  #Exclude rows with incomplete observation
-  if(any(is.na(data[,x1]))){stop("NA in x1")}
-  if(any(is.na(data[,x2]))){stop("NA in x2")}
   
-  notexclude = which(apply(data[c(numeric.covari,factor.covari)], 1, function(x){all(!is.na(x))}))
-  temp.data=data[notexclude,]
+  dataset <- data
+  
+  if(any(class(dataset) %in% "dataset.table")){setDF(dataset)}
+  numeric.covari = names(dataset[covari])[sapply(dataset[covari],is.numeric)]
+  factor.covari =  names(dataset[covari])[sapply(dataset[covari],is.factor)]
+  factor.covari = c(factor.covari,
+                    names(dataset[covari])[sapply(dataset[covari],is.character)])
+  #Exclude rows with incomplete observation
+  if(any(is.na(dataset[,x1]))){stop("NA in x1")}
+  if(any(is.na(dataset[,x2]))){stop("NA in x2")}
+  
+  notexclude = which(apply(dataset[c(numeric.covari,factor.covari)], 1, function(x){all(!is.na(x))}))
+  temp.dataset=dataset[notexclude,]
   
   if(length(factor.covari)>0){  #Dummies variables 
-    temp.data = dummy_cols(temp.data, select_columns = factor.covari,
+    temp.dataset = dummy_cols(temp.dataset, select_columns = factor.covari,
                            remove_most_frequent_dummy = T, 
                            remove_selected_columns = T)
-    factor.covari = names(temp.data)[!names(temp.data) %in% names(data)]
+    factor.covari = names(temp.dataset)[!names(temp.dataset) %in% names(dataset)]
     #covariates for final model 
     cov=c(numeric.covari,factor.covari)
   }
   #Partial Sperman correlation 
-  result=data.frame(matrix(ncol=7, nrow=length(x1)))
+  result=dataset.frame(matrix(ncol=7, nrow=length(x1)))
   for(i in 1:length(x1)){
-    res=pcor.test(temp.data[,x1[i]],temp.data[,x2],temp.data[,cov],
+    res=pcor.test(temp.dataset[,x1[i]],temp.dataset[,x2],temp.dataset[,cov],
                   method = "spearman")
-    a=data.frame(x=x1[i],y=x2,
+    a=dataset.frame(x=x1[i],y=x2,
                  estimate=res$estimate,
                  p.value=res$p.value,
                  N=res$n,
