bf715c8902b9aa446fbecff85a9182ce9f360810
gbaldanzi <gabrielbaldanzi@gmail.com>
2021-11-18 15:13:53 +0100

    Removed the script for Heatmp and Boxplots of difference between the sleep apnea severity groups
    Preparing scripts for the final manuscript. Previous figures like a Heatmap of BC are removed from the scripts. An old version is save as Script3_calc_bc_ahi_plots_20211118.R

---
+++ Script3_calc_beta/Archive/Script3_calc_bc_ahi_plots_20211118.R
@@ -0,0 +1,140 @@
+# Project: Sleep apnea and gut microbiota
+# Gabriel Baldanzi 
+
+# Descriptive Statistics 
+
+# This script will create plots and table describing beta
+#diversity in relation to AHI and OSA in participants with valid AHI
+
+  # Import BC
+  BC = fread('/home/baldanzi/Datasets/sleep_SCAPIS/OSA.BCmatrix.csv',sep=",")
+  BC = as.matrix(BC)
+  rownames(BC) = colnames(BC) 
+
+  # Import PCoA of BC 
+  load("/home/baldanzi/Datasets/sleep_SCAPIS/pc_BC")
+  
+  # pheno data 
+  valid.ahi <- pheno[valid.ahi=='yes',]
+
+  # Plotting the first 2 principal components 
+    # First: extracting the principal component value for every sample 
+      dat.plot=data.frame(pcoa.bray$vectors, OSAcat = valid.ahi$OSAcat)
+      
+    # Second: creating the scatter plot ####
+      
+      centroids <- aggregate(cbind(Axis.1,Axis.2)~OSAcat,dat.plot,mean)
+      f         <- function(z)sd(z)/sqrt(length(z)) # function to calculate std.err
+      se        <- aggregate(cbind(se.Axis.1=Axis.1,se.Axis.2=Axis.2)~OSAcat,dat.plot,f)
+      centroids <- merge(centroids,se, by="OSAcat") 
+      
+      
+      p4=ggplot(dat.plot, aes(x=Axis.1,y=Axis.2, color=OSAcat))+
+         # geom_point(size=1.1) +
+          geom_point(data=centroids, size=5)+
+          geom_errorbar(data=centroids,aes(ymin=Axis.2-se.Axis.2,ymax=Axis.2+se.Axis.2),width=0.001)+
+          geom_errorbarh(data=centroids,aes(xmin=Axis.1-se.Axis.1,xmax=Axis.1+se.Axis.1),height=0.0003) +
+          #stat_ellipse(type = "t", size=1.3) +
+         scale_color_manual(values=c("gray84","lightskyblue1","lightskyblue3","blue3")) +
+          ggtitle("Bray-curtis dissimilarity by sleep apnea severity") +
+          xlab(paste0("PCo1 \n (",round(100*pcoa.bray$values$Relative_eig[1],1),"% )")) +
+          ylab(paste0("PCo2 \n (",round(100*pcoa.bray$values$Relative_eig[2],1),"% )")) +
+          xlim(-0.12, 0.04) + ylim(-0.08, .08) +
+          theme_light()+
+          theme(axis.text.x = element_text(angle = 0),
+              plot.title = element_text(hjust = 0.5, face="bold", size=14),
+              legend.text = element_text(size=10))
+
+  ggsave("BC.OSAcat.png", plot = p4, device = "png", 
+         path = "/home/baldanzi/Sleep_apnea/Descriptive/")
+  
+  # Scatter plot - NoOSA vs Severe OSA ####
+ 
+  dat.plot2=dat.plot[dat.plot$OSAcat %in% c("no OSA", "Severe"),]
+  
+  p4=ggplot(dat.plot, aes(x=Axis.1,y=Axis.2, color=OSAcat))+
+    geom_point(size=1.1) +
+    stat_ellipse(type = "t", size=1.3,data = . %>% filter(OSAcat %in% c("no OSA", "Severe")), 
+                 aes(color=OSAcat)) +
+    scale_color_manual(values=c("gray84","white", "white", "blue3")) +
+    ggtitle("Bray-curtis dissimilarity - AHI") +
+    xlab(paste0("PCo1 \n (",round(100*pcoa.bray$values$Relative_eig[1],1),"% )")) +
+    ylab(paste0("PCo2 \n (",round(100*pcoa.bray$values$Relative_eig[2],1),"% )")) +
+    theme_light() +
+    theme(axis.text.x = element_text(angle = 0),
+          plot.title = element_text(hjust = 0.5, face="bold", size=12),
+          legend.text = element_text(size=10))
+  
+  ggsave("BC.OSAcat_noosa_severe.png", plot = p4, device = "png", 
+         path = "/home/baldanzi/Sleep_apnea/Descriptive/")
+
+  # BC heatmap - AHI ####
+
+  paletteLength <- 500
+  myColor <- colorRampPalette(c("white","indianred3"))(paletteLength)
+  # length(breaks) == length(paletteLength) + 1
+  # use floor and ceiling to deal with even/odd length pallettelengths
+  myBreaks <- c(seq(0, 0.5, length.out=ceiling(paletteLength/2) + 1), 
+                seq(1/paletteLength, 1, length.out=floor(paletteLength/2)))
+
+  annotation = data.frame(OSAcat = valid.ahi$OSAcat)
+  rownames(annotation) = colnames(BC)
+
+  # Heatmap
+  p5= pheatmap(BC,main="Heatmap",
+               legend_labels = "Bray-Curtis Dissimilarity",
+               color = myColor,
+               show_rownames = FALSE, show_colnames = FALSE,
+               # breaks = myBreaks,
+               cluster_rows = T,cluster_cols = T,
+               annotation_row = annotation,
+               annotation_col = annotation)
+
+  ggsave("BC.heatmap.png", plot = p5, device = "png", 
+       path = "/home/baldanzi/Sleep_apnea/Descriptive/")
+
+
+# Box plots - BC ####
+  BC = as.data.frame(BC)
+  BC$SCAPISid = colnames(BC)
+
+  # Merging with information on OSA cat 
+  a = c("SCAPISid","OSAcat")
+  BC2 = merge(BC, valid.ahi[,a,with=F], by="SCAPISid", all=T)
+  BC2$OSAcat=factor(BC2$OSAcat, levels=c("no OSA","Mild","Moderate","Severe"))
+
+  setDT(BC2)
+
+  # Box plot - BC between "no OSA" and the other OSA groups
+  a = c(BC2[OSAcat=="no OSA",SCAPISid],"SCAPISid","OSAcat")
+  BCnoOSA= BC2[,a,with=F]
+  BCnoOSA = BCnoOSA %>% gather(ID, distance, grep("5-", names(BCnoOSA), value = T))
+
+  bp1 = ggplot(BCnoOSA, aes(x=OSAcat,y=distance,fill=OSAcat)) +
+            geom_boxplot()+ xlab("Sleep apnea severity") + 
+            ggtitle("BC between 'no OSA' and other categories")+
+            theme(plot.title = element_text(hjust = .5 , size = 14,),
+                  axis.title = element_text(size=14),
+                  axis.text.x =  element_text(size=14),
+                  legend.position = "none")
+
+  ggsave('Boxplot_BC_noOSA.png', plot = bp1, path = "/home/baldanzi/Sleep_apnea/Descriptive/",
+       device = "png")
+
+# Box plot - BC  between "Severe" and the other OSA groups
+  a = c(BC2[OSAcat=="Severe",SCAPISid],"SCAPISid","OSAcat")
+  BCsevere= BC2[,a,with=F]
+  BCsevere = BCsevere %>% gather(ID, distance, grep("5-", names(BCsevere), value = T))
+
+  bp2 = ggplot(BCsevere, aes(x=OSAcat,y=distance,fill=OSAcat)) +
+              geom_boxplot() + xlab("Sleep apnea severity") +
+              ggtitle("BC between 'Severe' and other categories")+
+              theme(plot.title = element_text(hjust = .5 , size = 14,),
+                    axis.title = element_text(size=14),
+                    axis.text.x =  element_text(size=14),
+                    legend.position = "none")
+
+  ggsave('Boxplot_BC_severe.png', plot = bp2, 
+         path = "/home/baldanzi/Sleep_apnea/Descriptive/",
+         device = "png")
+
