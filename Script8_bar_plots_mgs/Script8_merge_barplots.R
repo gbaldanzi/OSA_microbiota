cdf778c514f2c8f73b913d8e58b185d20e35d381
gbaldanzi <gabrielbaldanzi@gmail.com>
2021-11-19 13:56:44 +0100

    Update Script8_merge_barplots.R
    Still in progress. Not a final version. I am trying to make one final grid with all plots. At the moment, the script creates two grids, one for AHI and one for T90

--- Script8_bar_plots_mgs/Script8_merge_barplots.R
+++ Script8_bar_plots_mgs/Script8_merge_barplots.R
@@ -12,20 +12,20 @@ output.plot = "/proj/nobackup/sens2019512/wharf/baldanzi/baldanzi-sens2019512/"
 
   plots.ahi <- readRDS(paste0(output.plot,'/bar_ahi/barplot_ahi.rds'))
 
-  plots.aligned = plot_grid(plotlist=plots.ahi,nrow=1)
+  plots.aligned1 = plot_grid(plotlist=plots.ahi,nrow=1)
 
-  title = ggdraw() + draw_label("Signature species correlated to AHI", fontface = 'bold',size=10) +  
+  title = ggdraw() + draw_label("Signature species associated to AHI", fontface = 'bold',size=10) +  
     theme(plot.margin = unit(c(0.2,0.2,0.2,0.2),"cm"))
   
   subtitle = ggdraw() + draw_label(subtitle, fontface = 'bold', size = 7) +  
     theme(plot.margin = unit(c(0,0,0,0),"cm"))
   
-  plots.aligned = plot_grid(title, subtitle,plots.aligned,ncol=1,rel_heights = c(0.08,0.05,1))
+  plots.aligned1 = plot_grid(title, subtitle,plots.aligned1,ncol=1,rel_heights = c(0.08,0.05,1))
 
-plots.aligned
+plots.aligned1
 
 
-  ggsave(file = "mergedbarplots_mgs_ahi.png", plot=plots.aligned, path = './bar_ahi/',width = 8.5,
+  ggsave(file = "mergedbarplots_mgs_ahi.png", plot=plots.aligned1, path = './bar_ahi/',width = 8.5,
          height = 2.5)
 
 
@@ -36,17 +36,17 @@ n.gr <- table(pheno[valid.t90=='yes',t90cat])
 
 subtitle <-paste0(paste0(names(n.gr),'=',n.gr,"; "),collapse = "")
 
-  plots.aligned = plot_grid(plotlist=plots.t90,nrow=4,ncol = 6)
+  plots.aligned2 = plot_grid(plotlist=plots.t90,nrow=4,ncol = 6)
 
-  title = ggdraw() + draw_label("Signature species correlated to T90", fontface = 'bold', size =10) +  
+  title = ggdraw() + draw_label("Signature species associated to T90", fontface = 'bold', size =10) +  
     theme(plot.margin = unit(c(0.4,0.2,0.4,0.2),"cm"))
 
   subtitle = ggdraw() + draw_label(subtitle, fontface = 'bold', size = 7) +  
     theme(plot.margin = unit(c(0,0,0,0),"cm"))
 
-  plots.aligned = plot_grid(title, subtitle,plots.aligned,ncol=1,rel_heights = c(0.08,0.06,4))
+  plots.aligned2 = plot_grid(title, subtitle,plots.aligned2,ncol=1,rel_heights = c(0.08,0.06,4))
 
-  plots.aligned
+  plots.aligned2
 
-  ggsave(file = "mergedbarplots_mgs_t90.png", plot=plots.aligned, path = './bar_t90/', width = 8.5, height=9.8)
+  ggsave(file = "mergedbarplots_mgs_t90.png", plot=plots.aligned2, path = './bar_t90/', width = 8.5, height=9.8)
 
