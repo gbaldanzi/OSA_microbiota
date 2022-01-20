b6dfc9a3478614a86c51f4e53780aef091bc2b6e
gbaldanzi <gabrielbaldanzi@gmail.com>
2022-01-20 13:17:55 +0100

    Update Script5.main.R

--- Script5_permanova/Script5.main.R
+++ Script5_permanova/Script5.main.R
@@ -15,9 +15,9 @@ pacman::p_load(data.table, vegan, ggplot2,parallel)
   
 
   
-  source('Script5_permanova/perma_osa_bc/perma_ahi_bc_main.R')
+  source('Script5_permanova/perma_ahi_bc/perma_ahi_bc_main.R')
 
-  source('Script5_permanova/perma_osa_bc/perma_odi_bc_main.R')
+  source('Script5_permanova/perma_odi_bc/perma_odi_bc_main.R')
 
   source('Script5_permanova/perma_t90_bc/perma_t90_bc_main.R')
   
