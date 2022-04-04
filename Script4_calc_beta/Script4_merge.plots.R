49f91a08396e5af7f6a3b094f71275935aaf3a2d
gbaldanzi <gabrielbaldanzi@gmail.com>
2022-04-04 13:29:07 +0200

    Update Script4_merge.plots.R
    Change the plot label from capital A,B,C to a,b,c

--- Script4_calc_beta/Script4_merge.plots.R
+++ Script4_calc_beta/Script4_merge.plots.R
@@ -15,7 +15,7 @@
   
   output.plot <- "/home/baldanzi/Sleep_apnea/Results/Plots"
 
-  pcoa.plot.merged <- plot_grid(NULL,NULL,NULL,p1,p2,p3,NULL,NULL,NULL,labels = c("","","","A","B","C"), label_size = 12, nrow=3,
+  pcoa.plot.merged <- plot_grid(NULL,NULL,NULL,p1,p2,p3,NULL,NULL,NULL,labels = c("","","","a","b","c"), label_size = 12, nrow=3,
                                 label_y = 0.97, ncol=3, rel_heights = c(.5,1,.4))
 
   ggsave("PCoA_sleepapnea.pdf", plot = pcoa.plot.merged, device = "pdf", 
